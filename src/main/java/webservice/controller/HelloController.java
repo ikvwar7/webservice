eec520b78f9ce44d5be8a37faf7903d635952182
kivshin <kivshin@phoenixit.ru>
2018-09-25 19:50:55 +0300

    исправил headers,старые комментарии, value через конструктор

--- src/main/java/webservice/controller/HelloController.java
+++ src/main/java/webservice/controller/HelloController.java
@@ -1,9 +1,6 @@
 package webservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +16,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    ResponseEntity<String> hello() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set("Content-Type", "application/text");
-        return new ResponseEntity<>(helloService.hello(), httpHeaders, HttpStatus.OK);
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = {"text/plain"})
+    String hello() {
+        return helloService.hello();
     }
 }
